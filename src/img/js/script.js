ddf577872f87e48d4261ccdd435a3e59ff973e5a
Márcio Gabriel Santos Silva <159598273+m-gabriel89@users.noreply.github.com>
2024-04-23 07:48:36 -0300

    Add files via upload

---
+++ src/img/js/script.js
@@ -0,0 +1,11 @@
+
+function addRegister() {
+    const email = document.getElementById('username')
+    const password = document.getElementById('password')
+
+    if(email.value == 'admin@admin.com', password.value == 1234) {
+        alert('Login successful')
+    } else {
+        alert('Login failed')
+    }
+}
